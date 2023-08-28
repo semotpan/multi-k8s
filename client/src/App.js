6bfca1ca5536ffd6afcd9d6895fbc8b9dd35dd39
Sergiu <semotpan@gmail.com>
2023-08-28 19:39:42 +0300

    small adjustment

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Fib Calulator
+            Fib Calulator version KUBERNETES!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
